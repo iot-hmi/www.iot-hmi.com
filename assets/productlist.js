bd8d352cfa12ae1b589864d107bc9d4a0796773c
Johnny Tsai <johnny.tsai@iot-hmi.com>
2021-11-26 10:41:27 +0800

    new design of product list

--- assets/productlist.js
+++ assets/productlist.js
@@ -1,2 +1,11 @@
 $(document).ready(function() {
+    $(".filter_cat").on("click", function () {
+        $(".itembox").hide();
+        $("."+$(this).attr("f_key")).show();
+        $(".filter_cat").removeClass("c_cat");
+        $(this).addClass("c_cat");
+        $("#itemcategory").html($(this).html());
+    });
+    $(".itembox").hide();
+    $(".f_CTP").show();
 });
