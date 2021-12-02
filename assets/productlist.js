afee9425567871ca9fbebe1cf96c5efb8386f4d8
Johnny Tsai <johnny.tsai@iot-hmi.com>
2021-12-02 12:56:24 +0800

    fix back to last page issue

--- assets/productlist.js
+++ assets/productlist.js
@@ -1,11 +1,26 @@
-$(document).ready(function() {
-    $(".filter_cat").on("click", function () {
-        $(".itembox").hide();
-        $("."+$(this).attr("f_key")).show();
-        $(".filter_cat").removeClass("c_cat");
-        $(this).addClass("c_cat");
-        $("#itemcategory").html($(this).html());
-    });
+var SelectCat = function (f_key) {
     $(".itembox").hide();
-    $(".f_CTP").show();
+    $("." + f_key).show();
+    $(".filter_cat").removeClass("c_cat");
+    $("#id_" + f_key).addClass("c_cat");
+    $("#itemcategory").html($("#id_" + f_key).html());
+    history.replaceState(null, null, "#" + f_key);
+}
+
+$(document).ready(function () {
+    $(".filter_cat").on("click", function () {
+        var f_key = $(this).attr("f_key");
+        SelectCat(f_key);
+    });
+    if (document.location.hash) {
+        var f_key = document.location.hash.substring(1);
+        if ($("#id_" + f_key).length) {
+            SelectCat(f_key);
+        } else {
+            SelectCat("f_CTP");
+        }
+    } else {
+        $(".itembox").hide();
+        $(".f_CTP").show();
+    }
 });
