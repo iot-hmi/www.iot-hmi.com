87c0a40faab854c4b41d5215378b2bd2bdd9ae73
Johnny Tsai <johnny.tsai@iot-hmi.com>
2022-03-24 12:21:35 +0800

    add magnus panel

---
+++ assets/magnuslist.js
@@ -0,0 +1,26 @@
+var SelectCat = function (f_key) {
+    $(".itembox").hide();
+    $("." + f_key).show();
+    $(".filter_cat").removeClass("c_cat");
+    $("#id_" + f_key).addClass("c_cat");
+    $("#itemcategory").html($("#id_" + f_key).html());
+    history.replaceState(null, null, "#" + f_key);
+}
+
+$(document).ready(function () {
+    $(".filter_cat").on("click", function () {
+        var f_key = $(this).attr("f_key");
+        SelectCat(f_key);
+    });
+    if (document.location.hash) {
+        var f_key = document.location.hash.substring(1);
+        if ($("#id_" + f_key).length) {
+            SelectCat(f_key);
+        } else {
+            SelectCat("f_COMMERCIAL");
+        }
+    } else {
+        $(".itembox").hide();
+        $(".f_COMMERCIAL").show();
+    }
+});
